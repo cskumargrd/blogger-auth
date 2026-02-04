0bd629234de0c91e029f44df7d111f24a3433059
cskumargrd <33696507+cskumargrd@users.noreply.github.com>
2026-02-04 15:32:16 +0530

    Update license-config.js

--- license-config.js
+++ license-config.js
@@ -46,6 +46,18 @@
             font-size: 13px !important;
             font-family: Verdana, Geneva, Arial, Helvetica, Sans-Serif !important;
         }
+    .csk-header {
+            -webkit-box-shadow: 1px 1px 4px 2px rgba(0, 0, 0, 0.3) !important;
+            box-shadow: 1px 1px 4px 2px rgba(0, 0, 0, 0.3) !important;
+            border-top: 5px solid #FA8E00 !important;
+            height: 250px !important;
+            background-image: url('images/header.jpg') !important;
+            background-position: center top !important;
+            background-repeat: no-repeat !important;
+            min-width: 700px !important;
+            max-width: 1728px !important;
+            width: 90% !important;
+        }
         /* Aapka footer aur baki security CSS yahan continue rahega */
         #${footerId} { display: inline-block !important; visibility: visible !important; }
     `;
