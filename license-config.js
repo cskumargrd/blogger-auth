14442b565366eae0cf12234d5b35e0e65da0c8a3
cskumargrd <33696507+cskumargrd@users.noreply.github.com>
2026-02-04 15:11:44 +0530

    Update license-config.js

--- license-config.js
+++ license-config.js
@@ -1,7 +1,7 @@
 (function() {
     // 1. PREMIUM DOMAINS LIST
     var premiumDomains = [
-        "cskumargrd.blogspot.com", // Aapka apna domain
+        "chandrashekharkumartech.blogspot.com", // Aapka apna domain
         "client-site.com"
     ];
 
